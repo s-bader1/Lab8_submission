fdda863b98c1c8445afa8f9a3733978aa120c296
s-bader1 <sbader@ualbertac.ca>
2025-10-28 16:37:42 -0600

    adding failed test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -1,7 +1,23 @@
 package com.example.lab08;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
 public class CustomListTest {
+    private CustomList list;
+
+    @BeforeEach
+    public void createList() {
+        list = new CustomList(null, new ArrayList<City>());
+        @Test
+        public void hasCityTest() {
+            list.addCity(new City("Halifax", "NS"));
+            assertEquals(list.hasCity(), true);
+        }
+
+    }
 
 }
