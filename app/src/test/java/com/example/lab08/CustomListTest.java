f5933f08a68e9a5f9074209748fc562afd63f3e9
s-bader1 <sbader@ualbertac.ca>
2025-10-28 17:15:01 -0600

    added tests for deleteCity() and countCitites() before implemtation - shoudl result in failed test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -9,18 +9,34 @@ import java.util.ArrayList;
 public class CustomListTest {
     private CustomList list;
     private City edmonton;
+    private City halifax;
 
 
     @BeforeEach
     public void createList() {
         list = new CustomList();
         edmonton = new City("Edmonton", "AB");
+        halifax = new City("Halifax", "NS");
     }
 
     @Test
     public void hasCityTest() {
         list.addCity(edmonton);
         assertEquals(list.hasCity(edmonton), true);
+    }
+
+    @Test
+    public void deleteCityTest(){
+        list.addCity(edmonton);
+        list.deleteCity(edmonton);
+        assertEquals(list.hasCity(edmonton), false);
+    }
+
+    @Test
+    public void countCityTest(){
+        list.addCity(edmonton);
+        list.addCity(halifax);
+        assertEquals(list.countCities(), 2);
     }
 
 }
